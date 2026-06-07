755581501c6acb6c7cd59804cae99db623d16d67
Llenoid <brandon1236perez@gmail.com>
2026-06-08 00:49:24 +0800

    feat: alternate screen, raw mode, RAII cleanup, coordinate based rendering, event loop, quit key

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
-use std::io::{self, stdout, Write};
+use std::{io::{self, stdout}};
 use crossterm::{
     execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen}
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode},
+    event::{
+        read, Event, KeyCode,
+    },
+    cursor::{MoveTo},
+    style::{Print}
 };
 
-struct OutputMessage {
-    message: String,
-}
-
-impl OutputMessage {
-    fn say_hello(&self) {
-        println!("{}", self.message);
-    }
-}
-
-impl Drop for OutputMessage {
+struct Terminal;
+impl Drop for Terminal {
     fn drop(&mut self) {
-        print!("> Dropping {}\r\n", self.message);
-        let _ = std::io::stdout().flush();
+        let _ = disable_raw_mode();
+        let _ = execute!(stdout(), LeaveAlternateScreen);
     }
 }
 
 fn main() -> io::Result<()> {
-    execute!(stdout(), EnterAlternateScreen)?;
-    let guard = OutputMessage {
-        message: "Welcome to the alternate screen!".to_string(),
-    };
-    guard.say_hello();
-    std::thread::sleep(std::time::Duration::from_secs(3));
-    drop(guard);
-    execute!(stdout(), LeaveAlternateScreen)?;
+    let message = "Welcome to the alternate screen!".to_string();
+    let _ = execute!(stdout(), EnterAlternateScreen, MoveTo(2, 2), Print(&message))?;
+    enable_raw_mode()?;
+    let _terminal = Terminal;
+    let mut i: u16 = 0;
+    loop {
+        if let Event::Key(key_event) = read()? {
+            i += 2;
+            let distance = 2 + i;
+            let _ = execute!(stdout(), MoveTo(2, distance), Print(format!("{:?}", key_event)));
+            if key_event.code == KeyCode::Char('q') {
+                let _ = execute!(stdout(), MoveTo(2, distance + 2), Print(format!("Stopping program: {:?}", key_event)));
+                break;
+            }
+        }
+    }
     Ok(())
 }
