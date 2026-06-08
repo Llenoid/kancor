591b9dddaffd839c2e93d1bdd9dc58c2aed9d558
Llenoid <brandon1236perez@gmail.com>
2026-06-08 21:23:13 +0800

    feat: render loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::{io::{self, stdout}};
 use crossterm::{
     execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode, Clear, ClearType, size},
     event::{
-        read, Event, KeyCode,
+        read, Event, KeyCode, KeyEvent
     },
     cursor::{MoveTo},
     style::{Print}
@@ -19,20 +19,36 @@ impl Drop for Terminal {
 
 fn main() -> io::Result<()> {
     let message = "Welcome to the alternate screen!".to_string();
-    let _ = execute!(stdout(), EnterAlternateScreen, MoveTo(2, 2), Print(&message))?;
     enable_raw_mode()?;
     let _terminal = Terminal;
-    let mut i: u16 = 0;
+    let _ = execute!(stdout(), EnterAlternateScreen)?;
+    render(&message)?;
     loop {
         if let Event::Key(key_event) = read()? {
-            i += 2;
-            let distance = 2 + i;
-            let _ = execute!(stdout(), MoveTo(2, distance), Print(format!("{:?}", key_event)));
+            let distance = 2;
+            render(&message)?;
+            render_key_event(key_event, distance)?;
             if key_event.code == KeyCode::Char('q') {
-                let _ = execute!(stdout(), MoveTo(2, distance + 2), Print(format!("Stopping program: {:?}", key_event)));
+                // execute!(stdout(), MoveTo(2, distance + 2), Print(format!("Stopping program: {:?}", key_event)))?;
+                render_key_event(key_event, distance + 2)?;
                 break;
             }
         }
     }
+    Ok(())
+}
+
+fn render(message: &str) -> io::Result<()> {
+    let (w, h) = size()?;
+    let mes_len = (&message.len() / 2) as u16;
+    let centered_height = h / 2;
+    let centered_width = w / 2;
+    let centered_text_width = centered_width - mes_len;
+    execute!(stdout(), Clear(ClearType::All), MoveTo(centered_text_width, centered_height), Print(&message))?;
+    Ok(())
+}
+
+fn render_key_event(key_event: KeyEvent, distance: u16) -> io::Result<()> {
+    execute!(stdout(), MoveTo(2, distance), Print(format!("{:?}", key_event)))?;
     Ok(())
 }
